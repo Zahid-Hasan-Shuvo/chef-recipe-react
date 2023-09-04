bbd7f210805987e685db372c05147798e2795efb
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-09-04 22:08:13 +0600

    login-logout-manage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import  { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../providers/AuthProviders'
 
 const Header = () => {
+  const {user, logOut} =useContext(AuthContext);
+  console.log(user);
+
+const handleLogOut =()=>{
+logOut()
+.then()
+.catch()
+
+}
+
   return (
     <div>
 <div className="navbar bg-base-100">
@@ -10,31 +21,31 @@ const Header = () => {
 
     <div >
     <Link to='/' className='px-5 '>Home</Link>
+    <Link to='/blog' className='px-5 '>Blog</Link>
     <Link className='px-5 ' to='/register'>Register</Link>
-    <Link  className='px-5'to='/login'>Login</Link>
-    </div>
-    
-  </div>
-  <div className="flex-none">
-    
-    <div className="dropdown dropdown-end">
-      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+   
+{ user ? <> 
+  <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <span> {user.photoUrl} </span>
         </div>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
+<button onClick={handleLogOut} className='px-5' > signout</button>
+ </>:
+ <Link className='px-5 ' to='/login'>Login</Link>
+
+}
+
+   
+   
+       
+
     </div>
+    
   </div>
+  
+    
+  
 </div>
 
     </div>
--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -1,6 +1,6 @@
-import  { createContext, useState } from 'react'
+import  { createContext, useEffect, useState } from 'react'
 import app from '../firebase/firebase.config';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth"
 
 
 export const AuthContext = createContext(null);
@@ -8,7 +8,7 @@ export const AuthContext = createContext(null);
 const auth =getAuth(app)
 
 
-  const AuthProviders = (  {children}) => {
+  const AuthProviders = ({children}) => {
     const [user, setUser]=useState(null)
 
 
@@ -21,10 +21,27 @@ const auth =getAuth(app)
       return signInWithEmailAndPassword(auth, email,password)
     }
 
+    const logOut = ()=>{
+      return signOut(auth)
+    }
+
+    useEffect( ()=>{
+    const unsubscibe=  onAuthStateChanged(auth, currentUser=>{
+        console.log('auth state change', currentUser);
+        setUser(currentUser)
+      })
+
+      return()=>{
+        unsubscibe()
+      }
+
+    }, [])
+
     const authInfo={
       user,
       createUser,
-      signIn
+      signIn,
+      logOut
     }
   return (
     <AuthContext.Provider value={authInfo}>
